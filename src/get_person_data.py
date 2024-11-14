303c2c03d07ed8728e0a287dc03ce1b21fe4d52f
eduardnicusor <theredpowergg@gmail.com>
2024-11-14 20:44:10 +0200

    initial commit

---
+++ src/get_person_data.py
@@ -0,0 +1,23 @@
+"""Get person data"""
+
+DATA = {
+    111: {
+        "name": "Dan",
+        "age": 20,
+    },
+    112: {
+        "name": "Maria",
+        "age": 33,
+    },
+}
+
+
+
+def get_name_and_age(person_id):
+    """Returns person name and age"""
+
+    return DATA[person_id]
+
+if __name__ == "__main__":
+    person = get_name_and_age(111)
+    print(person)
