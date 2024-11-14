b25afc49d6b008d6c70c1a61fe4033dff1f5c006
eduardnicusor <theredpowergg@gmail.com>
2024-11-14 21:06:24 +0200

    developed read_data function

--- src/get_person_data.py
+++ src/get_person_data.py
@@ -21,8 +21,14 @@ def get_name_and_age(person_id):
 
 def save_data(file, person_data):
     """Save person data to file"""
-    with open(file, "w"):
+    with open(file, "w") as fw
         fw.write(person_data)
+
+
+def read_data(file):
+    """Return a list with all rows from file"""
+    with open(file, "r") as fr:
+        content = fr.readlines()
 
 if __name__ == "__main__":
     try:
